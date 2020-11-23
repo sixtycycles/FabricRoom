3f0e68b32cd65dc1bd554b7e20be6f706c8df812
Rod O'Connor <sixtycycles@gmail.com>
2020-11-23 06:46:07 +0000

    added more tests. fix static files.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## Fabric Room ###
 ### this is the code to render [60hz.dev](https://60hz.dev) ###
-i dont know what its for yet. right now, its for FUN! 
+#### i dont know what its for yet. ####
--- blog/tests.py
+++ blog/tests.py
@@ -4,7 +4,6 @@ from django.urls import reverse
 from blog.models import Post
 
 
-
 class BlogTest(TestCase):
 
     def setUp(self):
@@ -41,9 +40,9 @@ class BlogTest(TestCase):
         self.assertTemplateUsed(response, 'home.html')
 
     def test_post_detail_view(self):
-        # not sure why self.post from setUp() is put in the second index but w/e. 
+        # not sure why self.post from setUp() is put in the second index but w/e.
         # print(f'the value of "id" self.post in this test case is: {self.post.id}')
-        #so im just asking if its there at the ID. 
+        # so im just asking if its there at the ID.
         response = self.client.get(f'/blog/post/{self.post.id}/')
         no_response = self.client.get('/blog/post/100000/')
         self.assertEqual(response.status_code, 200)
@@ -51,20 +50,49 @@ class BlogTest(TestCase):
         self.assertContains(response, 'test title')
         self.assertTemplateUsed(response, 'post_detail.html')
 
-    def test_if_user_can_edit_a_post_without_login(self):
-        response = self.client.get(f'/blog/post/{self.post.id}/update')
-        self.assertEqual(response.status_code, 403)
-
-    def test_if_user_can_create_a_new_post_without_login(self):
-        response = self.client.get('/blog/post/new/')
-        self.assertEqual(response.status_code, 403)
-
-    def test_if_user_can_edit_post_when_logged_in(self):
+    def test_post_create_view_when_logged_in(self):  # new
         self.client.login(username='testuser', password='secret')
-        response = self.client.get(f'/blog/post/{self.post.id}/update')
+        response = self.client.post(reverse('post_new'), {
+            'title': 'New title',
+            'body': 'New text',
+            'author': self.user.id,
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, 'New title')
+        self.assertEqual(Post.objects.last().body, 'New text')
+
+    def test_post_create_view_when_logged_out(self):  # new
+        response = self.client.post(reverse('post_new'), {
+            'title': 'New title',
+            'body': 'New text',
+            'author': self.user.id,
+        })
+        self.assertEqual(response.status_code, 403)
+
+    def test_post_update_view_when_logged_in(self):
+        self.client.login(username='testuser', password='secret')
+        response = self.client.post(reverse('post_update', args=f'{self.post.id}'), {
+            'title': 'Updated title',
+            'body': 'Updated text',
+        })
+
         self.assertEqual(response.status_code, 200)
 
-    def test_if_user_can_access_create_new_post_when_logged_in(self):
+    def test_post_update_view_when_logged_out(self):
+
+        response = self.client.post(
+            reverse('post_update', args=[f'{self.post.id}']),
+            {'title': 'Updated title', 'body': 'Updated text', }
+        )
+
+        self.assertEqual(response.status_code, 403)
+
+    def test_if_user_can_delete_a_new_post_without_login(self):
+        response = self.client.post(
+            reverse('post_delete', args=f'{self.post.id}'))
+        self.assertEqual(response.status_code, 403)
+
+    def test_if_user_can_delete_post_when_logged_in(self):
         self.client.login(username='testuser', password='secret')
-        response = self.client.get('/blog/post/new/')
+        response = self.client.get(f'/blog/post/{self.post.id}/delete')
         self.assertEqual(response.status_code, 200)
