50807e3ff612cdf2a491fbb1addf1f2bb5358d23
Rod O'Connor <sixtycycles@gmail.com>
2020-12-04 02:40:23 +0000

    a commit!

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
+from django.conf import settings
 
 
 class Post(models.Model):
 
     title = models.CharField(max_length=200)
     body = models.TextField()
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE,)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,)
     published = models.BooleanField(default=True)
 
     def __str__(self):
@@ -15,6 +17,8 @@ class Post(models.Model):
     
     def get_absolute_url(self):
         return reverse('post_detail', args=[str(self.id)])
+
+    
 
 class Tag(models.Model):
     tag_slug = models.SlugField(max_length=200, unique=True)
@@ -24,7 +28,7 @@ class Tag(models.Model):
 
 
 class Note(models.Model):
-    author = models.ForeignKey('auth.User', verbose_name="Author",
+    author = models.ForeignKey(get_user_model(), verbose_name="Author",
                                related_name="authors_notes", on_delete=models.CASCADE)
     link = models.URLField(blank=True)
     title = models.CharField(max_length=100, blank=True)
