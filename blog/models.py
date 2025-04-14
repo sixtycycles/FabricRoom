d97af45f17e39ea7579dbef72ce3f79b81ec4fdc
= <->
2025-04-14 12:25:05 -0700

    again - readonly ellimiator

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse
-form django.utils import timezone
+from django.utils import timezone
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.conf import settings
