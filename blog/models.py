73eadb6a761f2c834cb5d6bca0bc042795f005d2
= <->
2025-04-14 12:16:34 -0700

    added created field to blog posts

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,7 @@ class Post(models.Model):
         on_delete=models.CASCADE,
     )
     published = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
