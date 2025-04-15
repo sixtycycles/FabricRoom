9880ac2e0b69391f2e742442e448e4c38a8a76bd
= <->
2025-04-15 14:58:51 -0700

    adding template for logged in blog users

--- blog/views.py
+++ blog/views.py
@@ -10,15 +10,19 @@ from django_summernote.widgets import SummernoteWidget
 
 class BlogListView(ListView):
     model = Post
-    template_name = "post_list.html"
     context_object_name = "all_posts_list"
     paginate_by = 2
     ordering = ["-created_date"]
     queryset = Post.objects.filter(published=True).order_by("-created_date")
-    
+
+    def get_template_names(self):
+        if self.request.user.is_authenticated:
+            return ["post_list_logged_in.html"]
+        return ["post_list.html"]
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["all_posts_list"] = queryset
+        context["all_posts_list"] = self.get_queryset()
         return context
 
     def get_queryset(self):
