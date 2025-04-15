cb8d8b62d49ad5b820249be43d4e4f5dcca4467c
Rod <7862195+sixtycycles@users.noreply.github.com>
2025-04-14 20:34:30 -0700

    Update settings.py

--- FabricRoom/settings.py
+++ FabricRoom/settings.py
@@ -120,8 +120,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
 LANGUAGE_CODE = "en-us"
-TIME_ZONE = "UTC"
-# TIME_ZONE = 'America/Los_Angeles'
+# TIME_ZONE = "UTC"
+TIME_ZONE = 'America/Los_Angeles'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
