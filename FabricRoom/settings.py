5ad4b0f18b9a6d70e8e456199179a47d43660256
= <->
2025-09-05 15:41:33 -0700

    setup for simple deploy

--- FabricRoom/settings.py
+++ FabricRoom/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_simple_deploy",
     # my apps
     "main",
     "blog",
