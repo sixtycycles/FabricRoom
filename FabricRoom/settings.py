f138c4382007fb45412b697ee99aa2bfc7eea93e
= <->
2026-07-16 13:13:07 -0700

    fix uploads

--- FabricRoom/settings.py
+++ FabricRoom/settings.py
@@ -168,10 +168,11 @@ USE_TZ = True
 STATIC_URL = "/static/"
 if DEBUG:
     STATICFILES_DIRS = [BASE_DIR / "static"]
+    MEDIA_ROOT = BASE_DIR / "media"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = "/var/www/media"
 
 
 # ============================================================================
